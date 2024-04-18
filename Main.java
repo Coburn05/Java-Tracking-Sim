959fbdf95e486a96c4fb5e3a417cb0e9e7d66b29
Coburn05 <138069778+Coburn05@users.noreply.github.com>
2024-04-18 16:10:00 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 public class Main {
+    private static MainUI mainUI = new MainUI();
+
     public Main() {}
 
     public static void main(String[] args) {
-        MainUI mainUI = new MainUI();
         mainUI.draw(1280, 768);
+    }
+
+    public static MainUI getMainUI() {
+        return mainUI;
     }
 }
